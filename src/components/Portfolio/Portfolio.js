769e3b9df3153365e095cd87432963336133a713
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-24 21:21:34 +0300

    add Portfolio layout

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,7 @@
 import './Portfolio.css';
-import line from "../../images/landing__line.svg";
+import line from "../../images/portfolio__line.svg";
+import link from "../../images/portfolio__icon.svg";
+
 
 function Portfolio() {
     return(
@@ -7,18 +9,18 @@ function Portfolio() {
             <h5 className='portfolio__header'>Портфолио</h5>
             <div className='portfolio__item'>
                 <h4 className='portfolio__item-name'>Статичный сайт</h4>
-                <img className='portfolio__item-link'/>
+                <img className='portfolio__item-link' src={link} alt='Декоративная стрелка'/>
             </div>
             <img className='portfolio__line' src={line} alt='Декоративная линия'/>
             <div className='portfolio__item'>
                 <h4 className='portfolio__item-name'>Адаптивный сайт</h4>
-                <img className='portfolio__item-link'/>
+                <img className='portfolio__item-link' src={link} alt='Декоративная стрелка'/>
             </div>
             <img className='portfolio__line' src={line} alt='Декоративная линия'/>
             <div className='portfolio__item'>
-                <h4 className='portfolio__item-name'>Одностраничное приложение</h4>
-                <img className='portfolio__item-link'/>
-            </div>
+                 <h4 className='portfolio__item-name'>Одностраничное приложение</h4>
+                 <img className='portfolio__item-link' src={link} alt='Декоративная стрелка'/>
+             </div>
         </section>
     );
 }
