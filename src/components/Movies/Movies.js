cdc21638a0ac2686c8711a1c02db0988c56d27e5
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-25 20:03:36 +0300

    add Preloader Layout

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -7,7 +7,7 @@ function Movies() {
     return(
         <main className="movies">
             <SearchForm/>
-            {/*<Preloader/>*/}
+            <Preloader/>
             {/*<MoviesCardList/>*/}
             {/*<MoviesCard/>*/}
         </main>
