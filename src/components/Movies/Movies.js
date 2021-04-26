56320c2751d26c24d5d7cf7764b961adff56f8cc
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-26 22:17:04 +0300

    add cardList Layout

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,12 @@
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import MoviesCard from '../MoviesCard/MoviesCard';
 
 function Movies() {
     return(
         <main className="movies">
-            {/*<SearchForm/>*/}
-            {/*<Preloader/>*/}
+            <SearchForm/>
+            <Preloader/>
             <MoviesCardList/>
         </main>
     )
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,12 +4,21 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 function MoviesCardList() {
     return(
         <section className='movies-card-list'>
-            <ul className='movies-cards'>
+            <ul className='movies-card-list__elements'>
+                <MoviesCard/>
+                <MoviesCard/>
+                <MoviesCard/>
+                <MoviesCard/>
+                <MoviesCard/>
+                <MoviesCard/>
+                <MoviesCard/>
+                <MoviesCard/>
                 <MoviesCard/>
                 <MoviesCard/>
                 <MoviesCard/>
                 <MoviesCard/>
             </ul>
+            <button className='movies-card-list__load-button'>Ещё</button>
         </section>
     );
 }
