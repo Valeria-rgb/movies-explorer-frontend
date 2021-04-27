c278ada8629258c63fc66476287e3f77025095ec
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-27 17:31:21 +0300

    add profile layout

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,23 @@
-function Profile() {
+import '../Profile/Profile.css';
 
+function Profile() {
+    return(
+        <section className='profile'>
+            <h1 className='profile__greeting'>Привет, Виталий!</h1>
+            <form className='profile__form'>
+                <div className='profile__container'>
+                    <p className='profile__title'>Имя</p>
+                    <input className='profile__input'/>
+                </div>
+                <div className='profile__container'>
+                    <p className='profile__title'>E-mail</p>
+                    <input className='profile__input'/>
+                </div>
+                <p className='profile__button'>Редактировать</p>
+                <p className='profile__button'>Выйти из аккаунта</p>
+            </form>
+        </section>
+    );
 }
+
 export default Profile;
