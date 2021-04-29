74b4f89ab23fe003735df3381a83ebbdab9862ac
Valeria Shegolkova <lera.shapranova@yandex.ru>
2021-04-29 18:44:01 +0300

    add header layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import './App.css';
+import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
-import NotFoundWindow from "../NotFoundWindow/NotFoundWindow";
 
 function App() {
   return (
     <div className="app">
+        <Header/>
         <Switch>
       <Route path="/signup">
         <Register/>
